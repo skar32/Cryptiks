7e41f8c385087d3d94f7b07b636ec75035c04d3e
ZealousElite120 <skar32@gatech.edu>
2020-10-20 12:04:59 -0400

    Added scene to test shift tile mechanic

---
+++ Assets/Scripts/Grid.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class Grid 
+{
+    private int columns;
+    private int rows;
+    private float cellSize;
+    private Vector3 originPosition;
+    private string[,] gridArray;
+    private TextMesh[,] debugTextArray;
+    private string[] letters;
+
+    public Grid(int columns, int rows, float cellSize, Vector3 originPosition, string[] letters) {
+        this.columns = columns;
+        this.rows = rows;
+        this.cellSize = cellSize;
+        this.originPosition = originPosition;
+        this.letters = letters;
+        gridArray = new string[columns, rows];
+        debugTextArray = new TextMesh[columns, rows];
+
+         int count = 0;
+         for (int x = 0; x < gridArray.GetLength(0); x++ ) {
+            for (int y = 0; y < gridArray.GetLength(1); y++ ) {
+                gridArray[x, y] = letters[count];
+                count++;
+            }
+        }
+
+        count = 0;
+        for (int x = 0; x < gridArray.GetLength(0); x++ ) {
+            for (int y = 0; y < gridArray.GetLength(1); y++ ) {
+                debugTextArray[x, y] = UtilsClass.CreateWorldText(letters[count], null, GetWorldPosition(x, y) + new Vector3(cellSize, cellSize) * 0.5f, 20, Color.white, TextAnchor.MiddleCenter);
+                count++;
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 10000f);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 10000f);
+            }
+        }
+        Debug.DrawLine(GetWorldPosition(0, rows), GetWorldPosition(columns, rows), Color.white, 10000f);
+        Debug.DrawLine(GetWorldPosition(columns, 0), GetWorldPosition(columns, rows), Color.white, 10000f);
+    }
+
+    public Vector3 GetWorldPosition(float x, float y) {
+        return new Vector3(x, y) * cellSize + originPosition;
+    }
+
+    private void GetXY(Vector3 worldPosition, out int x, out int y) {
+        x = Mathf.FloorToInt((worldPosition - originPosition).x / cellSize);
+        y = Mathf.FloorToInt((worldPosition - originPosition).y / cellSize);
+    }
+
+    public void SetValue(int x, int y, string value) {
+        gridArray[x, y] = value;
+        debugTextArray[x, y].text = gridArray[x, y].ToString();
+    }
+
+    public void SetValue(Vector3 worldPosition, string value) {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        SetValue(x, y, value);
+    }
+
+    public void ShiftRight(Vector3 worldPosition) {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        string last = gridArray[columns - 1, y];
+        for (int i = columns - 2; i >= 0; i--) {
+            string curr = gridArray[i, y];
+            SetValue(i + 1, y, curr);
+        }
+        SetValue(0, y, last);
+    }
+
+    public void ShiftLeft(Vector3 worldPosition) {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        string first = gridArray[0, y];
+        for (int i = 0; i < columns - 1; i++) {
+            string curr = gridArray[i + 1, y];
+            SetValue(i, y, curr);
+        }
+        SetValue(columns - 1, y, first);
+    }
+
+    public void ShiftUp(Vector3 worldPosition) {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        string last = gridArray[x, rows - 1];
+        for (int j = rows - 2; j >= 0; j--) {
+            string curr = gridArray[x, j];
+            SetValue(x, j + 1, curr);
+        }
+        SetValue(x, 0, last);
+    }
+
+    public void ShiftDown(Vector3 worldPosition) {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        string first = gridArray[x, 0];
+        for (int j = 0; j < rows - 1; j++) {
+            string curr = gridArray[x, j + 1];
+            SetValue(x, j, curr);
+        }
+        SetValue(x, rows - 1, first);
+    }
+
+    public string GetValue(int x, int y) {
+        return gridArray[x, y];
+    }
+
+    public string GetValue(Vector3 worldPosition) {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        return GetValue(x, y);
+    }
+
+}
---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+
+public class Testing : MonoBehaviour
+{
+    private Grid grid;
+    public string[] letters;
+    public int columns;
+    public int rows;
+    private bool mouseClicked = false;
+    private bool waiting = false;
+
+    private void Start() {
+        grid = new Grid(columns, rows, 10f, new Vector3(0, 0), letters);
+        Camera.main.transform.position = new Vector3((columns * 10f)/2, (rows * 10f)/2, -10);
+    }
+
+    private void Update() 
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            mouseClicked = true;
+        }
+        if (Input.GetMouseButtonUp(0)) {
+            mouseClicked = false;
+        }
+
+        if (Input.GetAxis("Mouse X") > 0.0 || Input.GetAxis("Mouse X") < 0.0) {
+            if (mouseClicked && Input.GetAxis("Mouse X") > 0.2 && !waiting) { // shifting right
+                Debug.Log("right");
+                waiting = true;
+                grid.ShiftRight(UtilsClass.GetMouseWorldPosition());
+                StartCoroutine(Wait());
+            } else if (mouseClicked && Input.GetAxis("Mouse X") < -0.2 && !waiting) { // shifting left
+                Debug.Log("left");
+                waiting = true;
+                grid.ShiftLeft(UtilsClass.GetMouseWorldPosition());
+                StartCoroutine(Wait());
+            }
+        } else if (Input.GetAxis("Mouse Y") > 0.0 || Input.GetAxis("Mouse Y") < 0.0) {
+            if (mouseClicked && Input.GetAxis("Mouse Y") > 0.2 && !waiting) { // shifting up
+                Debug.Log("up");
+                waiting = true;
+                grid.ShiftUp(UtilsClass.GetMouseWorldPosition());
+                StartCoroutine(Wait());
+            } else if (mouseClicked && Input.GetAxis("Mouse Y") < -0.2 && !waiting) { // shifting down
+                Debug.Log("down");
+                waiting = true;
+                grid.ShiftDown(UtilsClass.GetMouseWorldPosition());
+                StartCoroutine(Wait());
+            }
+        }
+    }
+
+    IEnumerator Wait() {
+        yield return new WaitForSeconds(0.3f);
+        waiting = false;
+    }
+}
