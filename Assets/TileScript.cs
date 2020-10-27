85abf37fc68ab5bebc108a8ea6872d10f539fa6c
ZealousElite120 <skar32@gatech.edu>
2020-10-27 16:57:58 -0400

    Made scrolling mechanic more fluid and added example of possible Mastermind-style highlighting

---
+++ Assets/TileScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ using UnityEngine.EventSystems;
+ using UnityEngine.UI;
+ using TMPro;
+
+public class TileScript : MonoBehaviour, IPointerEnterHandler
+{
+    public void OnPointerEnter(PointerEventData pointerEventData) 
+    {
+        Scroller.currDraggedTile = this.gameObject;
+    }
+}
