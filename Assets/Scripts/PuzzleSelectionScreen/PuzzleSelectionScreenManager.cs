229438a9e46f980629d9467dd2002be51ec526dc
David Du <daviddu2k6@gmail.com>
2020-12-02 19:14:18 -0600

    Stages change, missing icon changes

--- Assets/Scripts/PuzzleSelectionScreen/PuzzleSelectionScreenManager.cs
+++ Assets/Scripts/PuzzleSelectionScreen/PuzzleSelectionScreenManager.cs
@@ -9,6 +9,8 @@ public class PuzzleSelectionScreenManager : MonoBehaviour
 {
     public TMP_Text elementText, arcanaText;
     public GameObject elementsList, airArcanaList, fireArcanaList, waterArcanaList, earthArcanaList, readingsArcanaList;
+    public GameObject stageLocations;
+    public GameObject stagePrefab;
     public SimpleScrollSnap elementScroller, arcanaScroller;
     public SimpleScrollSnap[] arcanaScrollers;
     public static int currElementNumber = 0, currArcanaNumber = 0;
@@ -18,6 +20,36 @@ public class PuzzleSelectionScreenManager : MonoBehaviour
     private string currElementSelected, currArcanaSelected;
     private string lastElementSelected, lastArcanaSelected;
 
+    private string[][] airStageList =
+    {
+        new string[] { "Justice1", "Justice2", "Justice3", "Justice4", "" },
+        new string[] { "Wheel1", "Wheel2", "Wheel3", "Wheel4", "Wheel5" }
+    };
+
+    private string[][] fireStageList = 
+    {
+        new string[] { "Hanged1", "Hanged2", "Hanged3", "Hanged4", "" },
+        new string[] { "Devil1", "Devil2", "Devil3", "Devil4", "Devil5" }
+    };
+
+    private string[][] waterStageList =
+    {
+        new string[] { "Star1", "Star2", "Star3", "Star4", "" },
+        new string[] { "World1", "World2", "World3", "World4", "World5" }
+    };
+
+    private string[][] earthStageList =
+    {
+        new string[] { "Moon1", "Moon2", "Moon3", "Moon4", "" },
+        new string[] { "Sun1", "Sun2", "Sun3", "Sun4", "Sun5" }
+    };
+
+    private string[][] readingsStageList =
+    {
+        new string[] { "Lorem1", "Lorem2", "Lorem3", "Lorem4", "" },
+        new string[] { "Ipsum1", "Ipsum2", "Ipsum3", "Ipsum4", "Ipsum5" }
+    };
+
     void Start()
     {
         lastElementSelected = "Air";
@@ -25,6 +57,8 @@ public class PuzzleSelectionScreenManager : MonoBehaviour
         currArcanaList = airArcanaList;
         elementScroller.GoToPanel(currElementNumber);
         arcanaScroller.GoToPanel(currArcanaNumber);
+        removeStages();
+        generateStages(0, 0);
     }
 
     void Update()
@@ -133,6 +167,7 @@ public class PuzzleSelectionScreenManager : MonoBehaviour
 
     void changeArcana(string newArcana, string currElementText) {
         lastArcanaSelected = newArcana;
+        removeStages();
 
         switch (currElementText) {
             case "Air":
@@ -140,12 +175,12 @@ public class PuzzleSelectionScreenManager : MonoBehaviour
                     case "Justice":
                         Debug.Log("Justice is selected!");
                         currArcanaNumber = 0;
-                        // do stuff if Justice arcana is selected
+                        generateStages(0, currArcanaNumber);
                         break;
                     case "Wheel of Fortune":
                         currArcanaNumber = 1;
                         Debug.Log("Wheel of Fortune is selected!");
-                        // do stuff if Wheel of Fortune arcana is selected
+                        generateStages(0, currArcanaNumber);
                         break;
                     default:
                         Debug.Log("You got " + newArcana + " in " + currElementText);
@@ -157,12 +192,12 @@ public class PuzzleSelectionScreenManager : MonoBehaviour
                     case "Hanged Man":
                         Debug.Log("Hanged Man is selected!");
                         currArcanaNumber = 0;
-                        // do stuff if Justice arcana is selected
+                        generateStages(1, currArcanaNumber);
                         break;
                     case "Devil":
                         Debug.Log("Devil is selected!");
                         currArcanaNumber = 1;
-                        // do stuff if Wheel of Fortune arcana is selected
+                        generateStages(1, currArcanaNumber);
                         break;
                     default:
                         Debug.Log("You got " + newArcana + " in " + currElementText);
@@ -174,12 +209,12 @@ public class PuzzleSelectionScreenManager : MonoBehaviour
                     case "Star":
                         Debug.Log("Star is selected!");
                         currArcanaNumber = 0;
-                        // do stuff if Justice arcana is selected
+                        generateStages(2, currArcanaNumber);
                         break;
                     case "World":
                         Debug.Log("World is selected!");
                         currArcanaNumber = 1;
-                        // do stuff if Wheel of Fortune arcana is selected
+                        generateStages(2, currArcanaNumber);
                         break;
                     default:
                         Debug.Log("You got " + newArcana + " in " + currElementText);
@@ -191,12 +226,12 @@ public class PuzzleSelectionScreenManager : MonoBehaviour
                     case "Moon":
                         Debug.Log("Moon is selected!");
                         currArcanaNumber = 0;
-                        // do stuff if Justice arcana is selected
+                        generateStages(3, currArcanaNumber);
                         break;
                     case "Sun":
                         Debug.Log("Sun is selected!");
                         currArcanaNumber = 1;
-                        // do stuff if Wheel of Fortune arcana is selected
+                        generateStages(3, currArcanaNumber);
                         break;
                     default:
                         Debug.Log("You got " + newArcana + " in " + currElementText);
@@ -209,8 +244,74 @@ public class PuzzleSelectionScreenManager : MonoBehaviour
             default:
                 Debug.Log("bruh what");
                 break;
+        }    
+    }
+
+    void removeStages() {
+        foreach (Transform child in stageLocations.transform) {
+            GameObject.Destroy(child.gameObject);
         }
-        
+    }
+
+    void generateStages(int elementNo, int arcanaNo) {
+        string[] stageList;
+        switch (elementNo) {
+            case 0:
+                stageList = airStageList[arcanaNo];
+                break;
+            case 1:
+                stageList = fireStageList[arcanaNo];
+                break;
+            case 2:
+                stageList = waterStageList[arcanaNo];
+                break;
+            case 3:
+                stageList = earthStageList[arcanaNo];
+                break;
+            default:
+                Debug.Log("Probably in Readings");
+                stageList = new string[] {""};
+                break;
+        }
+        int numStages = 0;
+        foreach (string stage in stageList) {
+            if (stage.Length == 0) {
+                break;
+            }
+            numStages++;
+
+            GameObject newStage = Instantiate(stagePrefab);
+
+
+
+            TMP_Text stageTextComponent = getTextComponent(newStage);
+
+            if (stageTextComponent) {
+                stageTextComponent.text = stage;
+                newStage.transform.SetParent(stageLocations.transform);
+                newStage.transform.localScale = new Vector3(1f, 1f, 1f);
+            } else {
+                Debug.Log("Yo prefabs are broken");
+            }
+        }
+        int bottomHeight = 728 - numStages * 240;
+        RectTransform rt = stageLocations.GetComponent<RectTransform>();
+
+        if (bottomHeight < 0) {
+            rt.offsetMin = new Vector2(rt.offsetMin.x, bottomHeight);
+        } else {
+            rt.offsetMin = new Vector2(rt.offsetMin.x, 0);
+        }
+    }
+
+    TMP_Text getTextComponent(GameObject stage) {
+        foreach (Transform t in stage.transform) {
+            if (t.tag == "Title") {
+                return t.GetComponent<TMP_Text>();
+            }
+        }
+
+        return null;
     }
 
     public IEnumerator FadeTextToFullAlpha(float t, TMP_Text text)
