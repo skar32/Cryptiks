102774e9511c8740ad6a071955efe0995a363b87
ZealousElite120 <skar32@gatech.edu>
2020-12-06 12:53:44 -0500

    Added in the rest of the puzzles

--- Assets/Scripts/5x4/StageData5x4.cs
+++ Assets/Scripts/5x4/StageData5x4.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class StageData4x4 : MonoBehaviour
+public class StageData5x4 : MonoBehaviour
 {
     public int stageNumber;
     private PuzzleGameHandler gameHandler;
-    public GameObject Tile11, Tile12, Tile13, Tile14, Tile21, Tile22, Tile23, Tile24, Tile31, Tile32, Tile33, Tile34, Tile41, Tile42, Tile43, Tile44;
+    public GameObject Tile11, Tile12, Tile13, Tile14, Tile15, Tile21, Tile22, Tile23, Tile24, Tile25, Tile31, Tile32, Tile33, Tile34, Tile35, Tile41, Tile42, Tile43, Tile44, Tile45;
     public GameObject[] hangmanLetters;
     private GridManager gridManagerScript;
 
@@ -55,6 +55,10 @@ public class StageData4x4 : MonoBehaviour
         GameObject colTile14 = null;
         GameObject colTile15 = null;
         GameObject colTile16 = null;
+        GameObject colTile17 = null;
+        GameObject colTile18 = null;
+        GameObject colTile19 = null;
+        GameObject colTile20 = null;
 
         GameObject rowTile1 = null;
         GameObject rowTile2 = null;
@@ -72,6 +76,10 @@ public class StageData4x4 : MonoBehaviour
         GameObject rowTile14 = null;
         GameObject rowTile15 = null;
         GameObject rowTile16 = null;
+        GameObject rowTile17 = null;
+        GameObject rowTile18 = null;
+        GameObject rowTile19 = null;
+        GameObject rowTile20 = null;
 
         TMP_Text colTile1Text = null;
         TMP_Text colTile2Text = null;
@@ -89,6 +97,10 @@ public class StageData4x4 : MonoBehaviour
         TMP_Text colTile14Text = null;
         TMP_Text colTile15Text = null;
         TMP_Text colTile16Text = null;
+        TMP_Text colTile17Text = null;
+        TMP_Text colTile18Text = null;
+        TMP_Text colTile19Text = null;
+        TMP_Text colTile20Text = null;
 
         TMP_Text rowTile1Text = null;
         TMP_Text rowTile2Text = null;
@@ -106,56 +118,72 @@ public class StageData4x4 : MonoBehaviour
         TMP_Text rowTile14Text = null;
         TMP_Text rowTile15Text = null;
         TMP_Text rowTile16Text = null;
+        TMP_Text rowTile17Text = null;
+        TMP_Text rowTile18Text = null;
+        TMP_Text rowTile19Text = null;
+        TMP_Text rowTile20Text = null;
 
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile11.transform.position.x, Tile11.transform.position.y), 0.5f); // Tile11
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile11.transform.position.x, Tile11.transform.position.y), 0.3f); // Tile11
         FindTiles(results, out rowTile1Text, out colTile1Text, out rowTile1, out colTile1);
 
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile12.transform.position.x, Tile12.transform.position.y), 0.5f); // Tile12
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile12.transform.position.x, Tile12.transform.position.y), 0.3f); // Tile12
         FindTiles(results, out rowTile2Text, out colTile2Text, out rowTile2, out colTile2);
 
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile13.transform.position.x, Tile13.transform.position.y), 0.5f); // Tile13
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile13.transform.position.x, Tile13.transform.position.y), 0.3f); // Tile13
         FindTiles(results, out rowTile3Text, out colTile3Text, out rowTile3, out colTile3);
 
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile14.transform.position.x, Tile14.transform.position.y), 0.5f); // Tile14
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile14.transform.position.x, Tile14.transform.position.y), 0.3f); // Tile14
         FindTiles(results, out rowTile4Text, out colTile4Text, out rowTile4, out colTile4);
 
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile21.transform.position.x, Tile21.transform.position.y), 0.5f); // Tile21
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile15.transform.position.x, Tile15.transform.position.y), 0.3f); // Tile15
         FindTiles(results, out rowTile5Text, out colTile5Text, out rowTile5, out colTile5);
 
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile22.transform.position.x, Tile22.transform.position.y), 0.5f); // Tile22
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile21.transform.position.x, Tile21.transform.position.y), 0.3f); // Tile21
         FindTiles(results, out rowTile6Text, out colTile6Text, out rowTile6, out colTile6);
 
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile23.transform.position.x, Tile23.transform.position.y), 0.5f); // lile23
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile22.transform.position.x, Tile22.transform.position.y), 0.3f); // Tile22
         FindTiles(results, out rowTile7Text, out colTile7Text, out rowTile7, out colTile7);
 
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile24.transform.position.x, Tile24.transform.position.y), 0.5f); // Tile24
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile23.transform.position.x, Tile23.transform.position.y), 0.3f); // Tile23
         FindTiles(results, out rowTile8Text, out colTile8Text, out rowTile8, out colTile8);
 
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile31.transform.position.x, Tile31.transform.position.y), 0.5f); // Tile31
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile24.transform.position.x, Tile24.transform.position.y), 0.3f); // Tile24
         FindTiles(results, out rowTile9Text, out colTile9Text, out rowTile9, out colTile9);
-        
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile32.transform.position.x, Tile32.transform.position.y), 0.5f); // Tile32
+
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile25.transform.position.x, Tile25.transform.position.y), 0.3f); // Tile25
         FindTiles(results, out rowTile10Text, out colTile10Text, out rowTile10, out colTile10);
         
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile33.transform.position.x, Tile33.transform.position.y), 0.5f); // Tile33
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile31.transform.position.x, Tile31.transform.position.y), 0.3f); // Tile31
         FindTiles(results, out rowTile11Text, out colTile11Text, out rowTile11, out colTile11);
         
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile34.transform.position.x, Tile34.transform.position.y), 0.5f); // Tile34
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile32.transform.position.x, Tile32.transform.position.y), 0.3f); // Tile32
         FindTiles(results, out rowTile12Text, out colTile12Text, out rowTile12, out colTile12);
 
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile41.transform.position.x, Tile41.transform.position.y), 0.5f); // Tile41
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile33.transform.position.x, Tile33.transform.position.y), 0.3f); // Tile33
         FindTiles(results, out rowTile13Text, out colTile13Text, out rowTile13, out colTile13);
 
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile42.transform.position.x, Tile42.transform.position.y), 0.5f); // Tile42
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile34.transform.position.x, Tile34.transform.position.y), 0.3f); // Tile34
         FindTiles(results, out rowTile14Text, out colTile14Text, out rowTile14, out colTile14);
 
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile43.transform.position.x, Tile43.transform.position.y), 0.5f); // Tile43
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile35.transform.position.x, Tile35.transform.position.y), 0.3f); // Tile35
         FindTiles(results, out rowTile15Text, out colTile15Text, out rowTile15, out colTile15);
 
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile44.transform.position.x, Tile44.transform.position.y), 0.5f); // Tile44
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile41.transform.position.x, Tile41.transform.position.y), 0.3f); // Tile41
         FindTiles(results, out rowTile16Text, out colTile16Text, out rowTile16, out colTile16);
 
-        TMP_Text[] tileTexts = new TMP_Text[] {colTile1Text, colTile2Text, colTile3Text, colTile4Text, colTile5Text, colTile6Text, colTile7Text, colTile8Text, colTile9Text, colTile10Text, colTile11Text, colTile12Text, colTile13Text, colTile14Text, colTile5Text, colTile16Text};
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile42.transform.position.x, Tile42.transform.position.y), 0.3f); // Tile42
+        FindTiles(results, out rowTile17Text, out colTile17Text, out rowTile17, out colTile17);
+
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile43.transform.position.x, Tile43.transform.position.y), 0.3f); // Tile43
+        FindTiles(results, out rowTile18Text, out colTile18Text, out rowTile18, out colTile18);
+
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile44.transform.position.x, Tile44.transform.position.y), 0.3f); // Tile44
+        FindTiles(results, out rowTile19Text, out colTile19Text, out rowTile19, out colTile19);
+
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile45.transform.position.x, Tile45.transform.position.y), 0.3f); // Tile45
+        FindTiles(results, out rowTile20Text, out colTile20Text, out rowTile20, out colTile20);
+
+        TMP_Text[] tileTexts = new TMP_Text[] {colTile1Text, colTile2Text, colTile3Text, colTile4Text, colTile5Text, colTile6Text, colTile7Text, colTile8Text, colTile9Text, colTile10Text, colTile11Text, colTile12Text, colTile13Text, colTile14Text, colTile5Text, colTile16Text, colTile17Text, colTile18Text, colTile19Text, colTile20Text};
 
         int count = 0;
 
--- Assets/Scripts/6x5/StageData6x5.cs
+++ Assets/Scripts/6x5/StageData6x5.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class StageData5x5 : MonoBehaviour
+public class StageData6x5 : MonoBehaviour
 {
     public int stageNumber;
     private PuzzleGameHandler gameHandler;
-    public GameObject Tile11, Tile12, Tile13, Tile14, Tile15, Tile21, Tile22, Tile23, Tile24, Tile25, Tile31, Tile32, Tile33, Tile34, Tile35, Tile41, Tile42, Tile43, Tile44, Tile45, Tile51, Tile52, Tile53, Tile54, Tile55;
+    public GameObject Tile11, Tile12, Tile13, Tile14, Tile15, Tile21, Tile22, Tile23, Tile24, Tile25, Tile31, Tile32, Tile33, Tile34, Tile35, Tile41, Tile42, Tile43, Tile44, Tile45, Tile51, Tile52, Tile53, Tile54, Tile55, Tile61, Tile62, Tile63, Tile64, Tile65;
     public GameObject[] hangmanLetters;
     private GridManager gridManagerScript;
 
@@ -64,6 +64,11 @@ public class StageData5x5 : MonoBehaviour
         GameObject colTile23 = null;
         GameObject colTile24 = null;
         GameObject colTile25 = null;
+        GameObject colTile26 = null;
+        GameObject colTile27 = null;
+        GameObject colTile28 = null;
+        GameObject colTile29 = null;
+        GameObject colTile30 = null;
 
         GameObject rowTile1 = null;
         GameObject rowTile2 = null;
@@ -90,6 +95,11 @@ public class StageData5x5 : MonoBehaviour
         GameObject rowTile23 = null;
         GameObject rowTile24 = null;
         GameObject rowTile25 = null;
+        GameObject rowTile26 = null;
+        GameObject rowTile27 = null;
+        GameObject rowTile28 = null;
+        GameObject rowTile29 = null;
+        GameObject rowTile30 = null;
 
         TMP_Text colTile1Text = null;
         TMP_Text colTile2Text = null;
@@ -116,6 +126,11 @@ public class StageData5x5 : MonoBehaviour
         TMP_Text colTile23Text = null;
         TMP_Text colTile24Text = null;
         TMP_Text colTile25Text = null;
+        TMP_Text colTile26Text = null;
+        TMP_Text colTile27Text = null;
+        TMP_Text colTile28Text = null;
+        TMP_Text colTile29Text = null;
+        TMP_Text colTile30Text = null;
 
         TMP_Text rowTile1Text = null;
         TMP_Text rowTile2Text = null;
@@ -142,83 +157,103 @@ public class StageData5x5 : MonoBehaviour
         TMP_Text rowTile23Text = null;
         TMP_Text rowTile24Text = null;
         TMP_Text rowTile25Text = null;
+        TMP_Text rowTile26Text = null;
+        TMP_Text rowTile27Text = null;
+        TMP_Text rowTile28Text = null;
+        TMP_Text rowTile29Text = null;
+        TMP_Text rowTile30Text = null;
 
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile11.transform.position.x, Tile11.transform.position.y), 0.3f); // Tile11
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile11.transform.position.x, Tile11.transform.position.y), 0.05f); // Tile11
         FindTiles(results, out rowTile1Text, out colTile1Text, out rowTile1, out colTile1);
 
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile12.transform.position.x, Tile12.transform.position.y), 0.3f); // Tile12
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile12.transform.position.x, Tile12.transform.position.y), 0.05f); // Tile12
         FindTiles(results, out rowTile2Text, out colTile2Text, out rowTile2, out colTile2);
 
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile13.transform.position.x, Tile13.transform.position.y), 0.3f); // Tile13
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile13.transform.position.x, Tile13.transform.position.y), 0.05f); // Tile13
         FindTiles(results, out rowTile3Text, out colTile3Text, out rowTile3, out colTile3);
 
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile14.transform.position.x, Tile14.transform.position.y), 0.3f); // Tile14
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile14.transform.position.x, Tile14.transform.position.y), 0.05f); // Tile14
         FindTiles(results, out rowTile4Text, out colTile4Text, out rowTile4, out colTile4);
 
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile15.transform.position.x, Tile15.transform.position.y), 0.3f); // Tile15
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile15.transform.position.x, Tile15.transform.position.y), 0.05f); // Tile15
         FindTiles(results, out rowTile5Text, out colTile5Text, out rowTile5, out colTile5);
 
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile21.transform.position.x, Tile21.transform.position.y), 0.3f); // Tile21
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile21.transform.position.x, Tile21.transform.position.y), 0.05f); // Tile21
         FindTiles(results, out rowTile6Text, out colTile6Text, out rowTile6, out colTile6);
 
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile22.transform.position.x, Tile22.transform.position.y), 0.3f); // Tile22
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile22.transform.position.x, Tile22.transform.position.y), 0.05f); // Tile22
         FindTiles(results, out rowTile7Text, out colTile7Text, out rowTile7, out colTile7);
 
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile23.transform.position.x, Tile23.transform.position.y), 0.3f); // Tile23
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile23.transform.position.x, Tile23.transform.position.y), 0.05f); // Tile23
         FindTiles(results, out rowTile8Text, out colTile8Text, out rowTile8, out colTile8);
-
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile24.transform.position.x, Tile24.transform.position.y), 0.3f); // Tile24
+        
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile24.transform.position.x, Tile24.transform.position.y), 0.05f); // Tile24
         FindTiles(results, out rowTile9Text, out colTile9Text, out rowTile9, out colTile9);
         
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile25.transform.position.x, Tile25.transform.position.y), 0.3f); // Tile25
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile25.transform.position.x, Tile25.transform.position.y), 0.05f); // Tile25
         FindTiles(results, out rowTile10Text, out colTile10Text, out rowTile10, out colTile10);
-        
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile31.transform.position.x, Tile31.transform.position.y), 0.3f); // Tile31
+    
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile31.transform.position.x, Tile31.transform.position.y), 0.05f); // Tile31
         FindTiles(results, out rowTile11Text, out colTile11Text, out rowTile11, out colTile11);
-        
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile32.transform.position.x, Tile32.transform.position.y), 0.3f); // Tile32
+
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile32.transform.position.x, Tile32.transform.position.y), 0.05f); // Tile32
         FindTiles(results, out rowTile12Text, out colTile12Text, out rowTile12, out colTile12);
 
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile33.transform.position.x, Tile33.transform.position.y), 0.3f); // Tile33
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile33.transform.position.x, Tile33.transform.position.y), 0.05f); // Tile33
         FindTiles(results, out rowTile13Text, out colTile13Text, out rowTile13, out colTile13);
 
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile34.transform.position.x, Tile34.transform.position.y), 0.3f); // Tile34
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile34.transform.position.x, Tile34.transform.position.y), 0.05f); // Tile34
         FindTiles(results, out rowTile14Text, out colTile14Text, out rowTile14, out colTile14);
 
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile35.transform.position.x, Tile35.transform.position.y), 0.3f); // Tile35
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile35.transform.position.x, Tile35.transform.position.y), 0.05f); // Tile35
         FindTiles(results, out rowTile15Text, out colTile15Text, out rowTile15, out colTile15);
 
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile41.transform.position.x, Tile41.transform.position.y), 0.3f); // Tile41
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile41.transform.position.x, Tile41.transform.position.y), 0.05f); // Tile41
         FindTiles(results, out rowTile16Text, out colTile16Text, out rowTile16, out colTile16);
 
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile42.transform.position.x, Tile42.transform.position.y), 0.3f); // Tile42
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile42.transform.position.x, Tile42.transform.position.y), 0.05f); // Tile42
         FindTiles(results, out rowTile17Text, out colTile17Text, out rowTile17, out colTile17);
 
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile43.transform.position.x, Tile43.transform.position.y), 0.3f); // Tile43
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile43.transform.position.x, Tile43.transform.position.y), 0.05f); // Tile43
         FindTiles(results, out rowTile18Text, out colTile18Text, out rowTile18, out colTile18);
 
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile44.transform.position.x, Tile44.transform.position.y), 0.3f); // Tile44
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile44.transform.position.x, Tile44.transform.position.y), 0.05f); // Tile44
         FindTiles(results, out rowTile19Text, out colTile19Text, out rowTile19, out colTile19);
 
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile45.transform.position.x, Tile45.transform.position.y), 0.3f); // Tile45
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile45.transform.position.x, Tile45.transform.position.y), 0.05f); // Tile45
         FindTiles(results, out rowTile20Text, out colTile20Text, out rowTile20, out colTile20);
 
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile51.transform.position.x, Tile51.transform.position.y), 0.3f); // Tile51
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile51.transform.position.x, Tile51.transform.position.y), 0.05f); // Tile51
         FindTiles(results, out rowTile21Text, out colTile21Text, out rowTile21, out colTile21);
 
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile52.transform.position.x, Tile52.transform.position.y), 0.3f); // Tile52
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile52.transform.position.x, Tile52.transform.position.y), 0.05f); // Tile52
         FindTiles(results, out rowTile22Text, out colTile22Text, out rowTile22, out colTile22);
 
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile53.transform.position.x, Tile53.transform.position.y), 0.3f); // Tile53
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile53.transform.position.x, Tile53.transform.position.y), 0.05f); // Tile53
         FindTiles(results, out rowTile23Text, out colTile23Text, out rowTile23, out colTile23);
 
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile54.transform.position.x, Tile54.transform.position.y), 0.3f); // Tile54
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile54.transform.position.x, Tile54.transform.position.y), 0.05f); // Tile54
         FindTiles(results, out rowTile24Text, out colTile24Text, out rowTile24, out colTile24);
 
-        results = Physics2D.OverlapCircleAll(new Vector2(Tile55.transform.position.x, Tile55.transform.position.y), 0.3f); // Tile55
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile55.transform.position.x, Tile55.transform.position.y), 0.05f); // Tile55
         FindTiles(results, out rowTile25Text, out colTile25Text, out rowTile25, out colTile25);
 
-        TMP_Text[] tileTexts = new TMP_Text[] {colTile1Text, colTile2Text, colTile3Text, colTile4Text, colTile5Text, colTile6Text, colTile7Text, colTile8Text, colTile9Text, colTile10Text, colTile11Text, colTile12Text, colTile13Text, colTile14Text, colTile5Text, colTile16Text, colTile17Text, colTile18Text, colTile19Text, colTile20Text, colTile21Text, colTile22Text, colTile23Text, colTile24Text, colTile25Text};
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile61.transform.position.x, Tile61.transform.position.y), 0.05f); // Tile61
+        FindTiles(results, out rowTile26Text, out colTile26Text, out rowTile26, out colTile26);
+
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile62.transform.position.x, Tile62.transform.position.y), 0.05f); // Tile62
+        FindTiles(results, out rowTile27Text, out colTile27Text, out rowTile27, out colTile27);
+
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile63.transform.position.x, Tile63.transform.position.y), 0.05f); // Tile63
+        FindTiles(results, out rowTile28Text, out colTile28Text, out rowTile28, out colTile28);
+
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile64.transform.position.x, Tile64.transform.position.y), 0.05f); // Tile64
+        FindTiles(results, out rowTile29Text, out colTile29Text, out rowTile29, out colTile29);
+
+        results = Physics2D.OverlapCircleAll(new Vector2(Tile65.transform.position.x, Tile65.transform.position.y), 0.05f); // Tile65
+        FindTiles(results, out rowTile30Text, out colTile30Text, out rowTile30, out colTile30);
+
+        TMP_Text[] tileTexts = new TMP_Text[] {colTile1Text, colTile2Text, colTile3Text, colTile4Text, colTile5Text, colTile6Text, colTile7Text, colTile8Text, colTile9Text, colTile10Text, colTile11Text, colTile12Text, colTile13Text, colTile14Text, colTile5Text, colTile16Text, colTile17Text, colTile18Text, colTile19Text, colTile20Text, colTile21Text, colTile22Text, colTile23Text, colTile24Text, colTile25Text, colTile26Text, colTile27Text, colTile28Text, colTile29Text, colTile30Text};
 
         int count = 0;
 
